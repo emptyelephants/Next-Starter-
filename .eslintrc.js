0f21557a977e3f75bcfdd3c7f9e43f904bf24144
emptyelephants <emptyelephants@gmail.com>
2019-08-07 12:28:29 -0700

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'jsx-a11y',
+  ],
+  rules: {
+    "react/jsx-filename-extension" : 0,
+    "react/react-in-jsx-scope" : 0,
+    "react/prop-types" : 0,
+  },
+    
+
+};
---
+++ pages/index.js
@@ -0,0 +1,6 @@
+const Index = () => (
+  <p>Start Here</p>
+);
+
+
+export default Index;
